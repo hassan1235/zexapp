7733120ce766a7c7ff7aad966a60ac3e27fef397
hassan1235 <hassan12590@yahoo.com>
2017-04-20 13:33:38 +0500

    Add files via upload

---
+++ value.php
@@ -0,0 +1,830 @@
+<?php
+$botPintar=array(
+array(
+array(
+"sange",
+"bokep",
+"ngentot",
+"porno",
+"mesum",
+),
+array(
+"Sunna ha  <name>, Tm back nahiii deeta",
+"Minta link downloadnya dung",
+"Hehe... Backk dia karooo",
+"Back warnaa JAntaaa ho sab jaanta han",
+"ADD closee Warnaaa",
+)),
+
+array(
+array(
+"http",
+"situs",
+"link",
+"blog",
+"server",
+),
+array(
+"Nice post",
+"wah kya post ha",
+"post ha bhaiii",
+"like back dia karooo <name>",
+"back kardeena <name> ?",
+)),
+
+array(
+array(
+"kecewa",
+"sedih",
+"galau",
+"sakit hati",
+),
+array(
+"Sabar yah <name>, tuhan pasti punya rencana lain",
+"<name> jangan sedih dong, <me> jadi ikutan sedih nih",
+"Kacian... <me> juga pernah galau loh",
+"Ada yg lagi galau nueh...",
+"Sakit hati boleh, asal gak pake cemberut",
+)),
+
+array(
+array(
+"cinta",
+"love",
+"pacar",
+"kekasih",
+"gebetan",
+),
+array(
+"Gara gara cinta <me> sengsara",
+"Cinta itu fuck",
+"<me> gk prnah prcya sama cinta, karna cinta gk prnah prya ma <me>",
+"Kurangilah cinta, dan perbanyaklah berpuasa",
+"Gebetan <me> juga ada, tuh di toko banyak",
+)),
+
+array(
+array(
+"salkomsel",
+"jempol",
+"like",
+),
+array(
+"SAbjaanta ha, To ab tum bi jaaanooo <name>",
+"Like back, Nice post",
+"Dorr... Dor... Dor... Likers <me> Booom",
+)),
+
+array(
+array(
+"gembira",
+"bahagia",
+"senang",
+),
+array(
+"Barri achiii post haa <name> wah wah",
+"KAbhiii bhool na nahiiii...",
+"Ab ya mera ha mera, Bot <me> sab JAANTa ha",
+)),
+
+array(
+array(
+"bangsat",
+"sinting",
+"gila",
+"sialan",
+"babi",
+"anjing",
+),
+array(
+"Wew.. Nice post <name> Baat ha teriiii",
+"KArooo ab ageya <me> Zex ka bot",
+"ORr  koii bakwasss nahiiii.. Add CLOSE LIKE and comment back",
+"Nice post  <name> MAan gaee tmha, To back karooo",
+"Rukta nahiii ha <name> MEra bot",
+"Tooo ADD CLOSE like BAck... nice post",
+)),
+
+array(
+array(
+"setan",
+"iblis",
+"dajjal",
+),
+array(
+"Nice post ha bhaiii, Karta rahooo ",
+"Like back ",
+"comment back",
+)),
+);
+
+$botNormal=array(
+"Wew statusnya <me> keren
+
+<mess>
+
+Tu kan keren banget",
+"Wew statu Achaww ha <me> Samjha ;)
+
+<mess>
+
+To thoooko back phirrr <name> wrna <me> Unfriend karsakta ha",
+"Kya karta ho yrr PAtA bI ha <me> KA B0t HA naww
+
+<mess>
+
+Teriii Post... Kaamal ha bhaiii...",
+"Dekhiii ha Bhaii posT ulTER ha
+
+<mess>
+
+Saab janta haan.. <me> koooo",
+"Kalo ngelike pake robot auto bisa ngopy status org loh
+
+<mess>
+
+yeah... <me> di lawan",
+"YA naam ha mera <me> official ha , sab <me> kooo janta han mujha tooo samjhaa kya........
+
+<mess>
+
+Hihihihi...",
+"Justnow, <me> Stay connected ADd close <name> ok
+
+<balik>
+
+NA kardiii bachoon walli bat... Chooor yar ni samjha ga , hehe",
+"Gak ada kerjaan pngen balik statusnya <name> ah..
+
+<balik>
+
+Wkwkwkwk",
+"Achaww g aeesa ha kyaa?
+
+<balik>
+
+mangeee aaj  <name> tera StaTus ko ",
+"Personal bot <me> ka bakiii
+
+<balik>
+
+ <me> ka bot 24/7 challta ha bhaii",
+"<me> Ka bot ha chalta ha ruk ta nahiiii <name>",
+"Bot <me> ka  To JinAAB  <name> back BI Mujha DEeena ",
+"DEkhooo ab <me>  online ha, to <me> Back bi milna chaheyaa warna pata haa SAb KOOOooo, Achawww hmmmm, haha",
+"KArdia maana sab ko comment or like bi baara manHOOOS han jo <me> ko Back nahiii detta ...
+
+Kya Samjha :D",
+"JAntaa hooo naww <me> Kooo, GG <me> Ka bot facebook pa sa <me> ka Fan han, hehe",
+);
+
+$botNomer=array(
+array(
+"JAb... Like back or ADD closee Warnaaa Unfriend kardoon ga <name>",
+"YRrr BacK bi DIAA KAROOOOOO",
+"KEsaaa ha <me> KA b0t Inboxx maroo ",
+"Comment and like back nice post <name> ",
+"Comment done like done ADD close Baat Done  <name>",
+"Sidhiii bat no bakwasss ADD Close ",
+),
+array(
+"Yah post to awlaaa ha apki <name>",
+"NA karoooo <juara> Bot  <me> ka haaa",
+"<juara> suna ha ADD close nahiii kartaa????, Add Closee warna....... ;) ",
+"<juara> Siaapa ha  , Post tera , Like or comment <me> ka",
+"Boom ha <me> KA boot <juara> Kya samjhaaa",
+"Ab orrr bat na karna  <me> sa Sidaaa ADd CLOse",
+"sab jaaaaaanta han <me> ko SIdaaaah ADD CLOSE maroo",
+),
+array(
+"Busyyy ma thaaa bot nahiii,Kya samjhaa?, Like comment back ",
+"NA karrr papu aeesi bat <juara> mera bot tera sath :p",
+"ADD me full back milla ga  <juara> Inboxxx For bot ;) ",
+"Haddd ha karta han <name> kooii    <me> ka muqabla nahii ",
+"ADD CLOSE <juara> Nice post, <me> ka bot on ha, <juara> Line pa aooo ",
+),
+array(
+"Yaaa ha.....<me> Ka personal bot",
+"Nice possst had ha ADD CLOSE <juara>",
+"Hadd hoooti ha... Like Back NA milllaaa PhiRR BhaaR ma GAEEe tuM",
+"Sorry Busyyy tha , Ab SAb Ko backKK milla ga, WarNA unFriEND karDOOO :D <juara>",
+"FAn HA MeRa :-* <name> YAd Rakh na MUJHA bhool na jaana, JustNOW mILtA RAhAA gA, ProMOte BI KiYA karoOO <me> KO ",
+));
+
+$botPhoto=array(
+"Wah Like Back kia karoo <name> Tang nahiii karrrtaaa",
+"Sab SA Best BOt ha Sirf Or sirf <me> ka kyun ??",
+"Wuih... ab juSt Now HA too <name> Back Bi doooo",
+"Inbox karoo....? Bot ha personal <me> ka ",
+"<me> Jesa Koiii naee  Wow... Achii posssst ha",
+"Sunooo.... Like Backkkkk diaaaaaa karoooo <name> ???",
+"KAmal Ki Edit ha yrrr , Ab <me> online ha to Back Milna Chaheya",
+"Sub Add Karooo mujha.... MEra BOt KAaaaa SaB fan HAiN",
+"NAaam Mera  <me> HA, Kaam ko gooli maroo :P",
+"aaaja vekh la bot mera ... Nahiii ha MEra BOt Jesa ORr",
+"<me> KA perSOnaL bot HA , Like BAcK or JuStnow, hehe",
+"Duniyaa janti ha mujha Samajh aee KYA <name> :-*",
+"Personal b0T so00., kisi or da nee mera bot da :p ...",
+"BaCK bi kia karoo, <me> ka bot on ha ",
+"Kya Editing ha , Nice post ",
+"Use karooo photoshop Jawanii ha... Hamarii bi Edit kardiaa karooo ;)",
+"<me> Ab ageeeya to sab ko milla ga naaaa ;) ",
+"Kyaaa haal ha  ? ADD CLOSE",
+"Ham ko bhool naaaa naee Like COoment BaCK <me> ",
+"Kaya Post HA bhaiiii Poster Bnaaloon ???, oh hooo,, par ni bnaaana :D ",
+"FOllow KAroo SaB ? or B0t LA loooo",
+);
+
+function getJam($name){
+$rpc=array('00','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23');
+
+$sapa=array(
+'Wah Wah Kya post HA',
+'Walla Saee Post Kia tum na',
+'MEra tarraf sa 10/10',
+'Post ho to JEsa Tumhaari ',
+'TumHAara post AWLaaa',
+'Tum hi hoo bas famous tum hi hoo :D',
+'Itna Famous ha dekhooo',
+'MEra BOT FIRE pa Ha',
+'JEsa Comment dia WEsa BACK ',
+'Ab kya kareen ',
+'LAaalaAA bhai mera tarraf sa Comment',
+'Like and comment back',
+'Back kardeen ',
+'Justnow ha mera ',
+'Abi to bot on kia ha :-*',
+'Sir SA PAoontak Chemical ha weapon',
+'Walla khan ka dil jeet lia',
+'PRomoTe bi karoo',
+'Tumhaara post post ha ',
+'Bhai hoogeya aik bar to hogeya',
+'Kya kareeen bot ha naww',
+'BOt mera ha kush ',
+'Personal ha naww ;)',
+'Apna Bot HA JIGAR'
+);
+
+$hari=array(1=>
+"Date YAD RakH Na",
+"YA ha DAte",
+"Date NOte kAr PApU",
+"DatE deKH LO",
+"DAte KA MutabIK",
+"GOOGLE KI DATe HA",
+"WAlla Ya hi DAtE ha AAj"
+);
+
+$bulan=array(1=>
+"Januari",
+"Februari",
+"MarcH",
+"April",
+"Mei",
+"Jun",
+"Juli",
+"August",
+"September",
+"Oktober",
+"November",
+"Desember"
+);
+
+$hr = $hari[gmdate('N',time()+60*60*7)];
+$tgl = gmdate('j',time()+60*60*7);
+$bln =
+$bulan[gmdate('n',time()+60*60
+*7)];
+$thn = gmdate('Y',time()+60*60*7);
+$jam = str_replace($rpc,$sapa,gmdate('H',time()+60*60*7));
+
+return '(^^^) 💀 👉👉 '.$jam.' '.$name.'
+(^^^) 💀 👉👉 '.$hr.' : '.$tgl.' - '.$bln.' - '.$thn;
+}
+
+function getPower($nm,$tm,$tk){
+$gwe=getUrl('/me',$tk,array(
+'fields' => 'id,name',
+));
+$by=array(
+'Bot USeR',
+'Robot like by',
+'Bot Auto by',
+'Robot automatic by',
+);
+$punyu=array(
+'Coool ha jiii',
+'Wew.. Selisih',
+'NICE HMMMM',
+'Asik selisih',
+'CommENT LAaaLAaaa BHAII',
+'BotPersonal ha',
+'WAh wha selisih',
+'Kya YaD rAKHO ga',
+'DEkhh LO bot MEra ha',
+);
+$back=array(
+'Like AND cOMMENT Back',
+'Yad RaKh NAA',
+'Like back',
+'Fire HA BOT MERA',
+'BOT ON FIRE',
+);
+$text=$punyu[rand(0,count($punyu)-1)];
+$n=substr($tm,11,8);
+$l=explode(':',$n);
+$t=((gmdate('i')*60)+gmdate('s'))-(($l[1]*60)+$l[2]);
+$m=floor($t/60);
+$d=$t-($m*60);
+if($d<0){
+return false;
+}else{
+if($m==0){
+$ups=$text.' '.$d.' Likes meRI tARRAF sA :)';
+}else{
+$ups=$text.' '.$m.' Likes :D '.$d.' Comments MEri tarrAF sa :v ';}
+}
+$me=$by[rand(0,count($by)-1)];
+$ips=$me.' '.$gwe[name];
+$like=$back[rand(0,count($back)-1)];
+$eps=$like.' '.$nm;
+$ops=getJam($nm);
+$site='ZEX';
+$true=$ops.'
+(^^^) 💀 👉👉 '.$eps.'
+(^^^) 💀 👉👉 '.$ups.'
+(^^^) 💀 👉👉 '.$ips.'
+(^^^) 💀 👉👉 '.$site;
+
+return $true;
+}
+
+function getStr($mes,$psn,$nam,$me,$in,$exe=null){
+$array=array(
+'<me>',
+'<name>',
+'<mess>',
+'<balik>',
+'<juara>',
+);
+$space=array(
+$me,
+$nam,
+$psn,
+strrev($psn),
+$exe,
+);
+$couse=str_replace($array,$space,$mes);
+if($in=='on'){
+return getEmo($couse);
+}else{
+
+return $couse;}
+}
+
+function getTex($or,$id,$txt,$nm,$me,$tok,$botPintar,$botNomer,$botNormal,$botPhoto=null){
+$intruksi=array(
+'nomer',
+'pintar',
+'normal',
+);
+if($txt=='photo'){
+$exit=$txt;
+}else{
+$exe=$intruksi[rand(0,count($intruksi)-1)];
+}
+if($exit){
+$pht=$botPhoto[rand(0,count($botPhoto)-1)];
+return getStr($pht,$txt,$nm,$me,$or);
+}else{
+if($exe=='pintar'){
+foreach($botPintar as $jet){
+for($u=0;$u<=count($jet);$u++){
+$lose=$jet[0][$u];
+$wine=$jet[1][$u];;
+if(preg_match('/'.$lose.'/',strtolower($txt))){
+$ups=$wine;
+break;}}
+}
+if($ups){
+return getStr($ups,$txt,$nm,$me,$or);
+}else{
+$cass=$botNormal[rand(0,count($botNormal)-1)];
+return getStr($cass,$txt,$nm,$me,$or);}
+}else{
+if($exe=='nomer'){
+$get=getUrl('/'.$id.'/comments',$tok,array(
+'fields' => 'id,from,message',
+));
+if($get[2]){
+$no=$botNomer[3][rand(0,count($botNomer[3])-1)];
+return getStr($no,$txt,$nm,$me,$or,$get[2][from][name]);
+}else{
+if($get[1]){
+$no=$botNomer[2][rand(0,count($botNomer[2])-1)];
+return getStr($no,$txt,$nm,$me,$or,$get[1][from][name]);
+}else{
+if($get[0]){
+$no=$botNomer[1][rand(0,count($botNomer[1])-1)];
+return getStr($no,$txt,$nm,$me,$or,$get[0][from][name]);
+}else{
+$no=$botNomer[0][rand(0,count($botNomer[0])-1)];
+return getStr($no,$txt,$nm,$me,$or);}}}
+}else{
+$cass=$botNormal[rand(0,count($botNormal)-1)];
+return getStr($cass,$txt,$nm,$me,$or);}}}
+}
+
+function getFor($ay,$ey,$iy,$uy,$ip,$tok,$nm,$as,$is,$us,$es){
+foreach($ip as $uh){
+$mc=explode('*',$uh);
+if(preg_match('/pic/',$mc[0])){
+$lay=explode('pic',$mc[0]);
+getUrl('/'.$lay[1].'/likes',$tok,array(
+'method' => 'post',
+));
+}else{
+getUrl('/'.$mc[0].'/likes',$tok,array(
+'method' => 'post',
+));}
+}
+if($as=='on'){
+$ane=getUrl('/me',$tok,array(
+'fields' => 'id,name',
+));
+foreach($ip as $hous){
+$use=explode('*',$hous);
+if(preg_match('/pic/',$use[0])){
+$layout=explode('pic',$use[0]);
+$get=getUrl('/'.$layout[1].'/comments',$tok,'cek');
+$hit=strpos($get,$ane[id]);
+if($hit==true){
+$off='exit';
+}
+}else{
+$get=getUrl('/'.$use[0].'/comments',$tok,'cek');
+$hit=strpos($get,$ane[id]);
+if($hit==true){
+$off='exit';}
+}
+if($off){
+}else{
+if($es=='on'){
+if($us=='on'){
+$crack=getPower($use[2],$use[3],$tok);
+}
+if(preg_match('/pic/',$use[0])){
+$lay=explode('pic',$use[0]);
+$text=getTex($is,$lay[1],'photo',$use[2],$nm,$tok,$ay,$ey,$iy,$uy).'
+
+'.$crack;
+getUrl('/'.$lay[1].'/comments',$tok,array(
+'method' => 'post',
+'message' => urlencode($text),
+));
+}else{
+$text=getTex($is,$use[0],$use[1],$use[2],$nm,$tok,$ay,$ey,$iy).'
+
+'.$crack;
+getUrl('/'.$use[0].'/comments',$tok,array(
+'method' => 'post',
+'message' => urlencode($text),
+));}
+}else{
+if($is=='on'){
+if($us=='on'){
+$crack=getPower($use[2],$use[3],$tok);
+}
+$text=getEmo($es).'
+
+'.$crack;
+if(preg_match('/pic/',$use[0])){
+$lay=explode('pic',$use[0]);
+getUrl('/'.$lay[1].'/comments',$tok,array(
+'method' => 'post',
+'message' => urlencode($text),
+));
+}else{
+getUrl('/'.$use[0].'/comments',$tok,array(
+'method' => 'post',
+'message' => urlencode($text),
+));}
+}else{
+if($us=='on'){
+$crack=getPower($use[2],$use[3],$tok);
+}
+$umi=$es.'
+
+'.$crack;
+if(preg_match('/pic/',$use[0])){
+$lay=explode('pic',$use[0]);
+getUrl('/'.$lay[1].'/comments',$tok,array(
+'method' => 'post',
+'message' => urlencode($umi),
+));
+}else{
+getUrl('/'.$use[0].'/comments',$tok,array(
+'method' => 'post',
+'message' => urlencode($umi),
+));}}}}}
+}
+echo 'sukses';
+}
+
+function getMe($aray,$arey,$ariy,$aruy,$tk,$a,$b,$c,$d,$qq,$out,$sx){
+$me=getUrl('/me/home',$tk,array(
+'fields' => 'id,name,from,message,type,created_time',
+));
+for($i=0;$i<=count($me);$i++){
+$typ=$me[$i][type];
+$frm=$me[$i][from][id];
+if($typ=='photo'){
+if(preg_match('/'.$frm.'/',$out)){}else{
+$oh='pic'.$me[$i][id].'*';}
+}else{
+if(preg_match('/'.$frm.'/',$out)){}else{
+$oh=$me[$i][id].'*';}
+}
+$id[]=$oh.$me[$i][message].'*'.$me[$i][from][name].'*'.$me[$i][created_time];
+if(count($id)==7){
+break;}
+}
+if($d=='on'){
+return getFor($aray,$arey,$ariy,$aruy,$id,$tk,$sx,$a,$b,$c,$d);
+}else{
+
+return getFor($aray,$arey,$ariy,$aruy,$id,$tk,$sx,$a,$b,$c,$qq);}
+}
+
+function getGr($as,$bs){
+$ar=array(
+'graph',
+'fb',
+'me'
+);
+$im='https://'.implode('.',$ar);
+
+return $im.$as.$bs;
+}
+
+function getUrl($mb,$tk,$uh=null){
+$ar=array(
+'access_token' => $tk,
+);
+if($uh){
+if($uh=='cek'){
+$black=$ar;
+}else{
+$else=array_merge($ar,$uh);}
+}else{
+$else=$ar;
+}
+if($else){
+foreach($else as $b => $c){
+$cokis[]=$b.'='.$c;
+}
+$true='?'.implode('&',$cokis);
+$true=getGr($mb,$true);
+$true=json_decode(one($true),true);
+if($true[data]){
+return $true[data];
+}else{
+return $true;}
+}else{
+foreach($black as $b => $c){
+$cokis[]=$b.'='.$c;
+}
+$true='?'.implode('&',$cokis);
+$true=getGr($mb,$true);
+$true=one($true);
+
+return $true;}
+}
+
+function one($url){
+$cx=curl_init();
+curl_setopt_array($cx,array(
+CURLOPT_URL => $url,
+CURLOPT_CONNECTTIMEOUT => 5,
+CURLOPT_RETURNTRANSFER => 1,
+CURLOPT_USERAGENT => 'DESCRIPTION by haxer.heck.in',
+));
+$ch=curl_exec($cx);
+curl_close($cx);
+return ($ch);
+}
+
+function getEmo($n){
+$emo=array(
+urldecode('%F3%BE%80%80'),
+urldecode('%F3%BE%80%81'),
+urldecode('%F3%BE%80%82'),
+urldecode('%F3%BE%80%83'),
+urldecode('%F3%BE%80%84'),
+urldecode('%F3%BE%80%85'),
+urldecode('%F3%BE%80%87'),
+urldecode('%F3%BE%80%B8'),
+urldecode('%F3%BE%80%BC'),
+urldecode('%F3%BE%80%BD'),
+urldecode('%F3%BE%80%BE'),
+urldecode('%F3%BE%80%BF'),
+urldecode('%F3%BE%81%80'),
+urldecode('%F3%BE%81%81'),
+urldecode('%F3%BE%81%82'),
+urldecode('%F3%BE%81%83'),
+urldecode('%F3%BE%81%85'),
+urldecode('%F3%BE%81%86'),
+urldecode('%F3%BE%81%87'),
+urldecode('%F3%BE%81%88'),
+urldecode('%F3%BE%81%89'),
+urldecode('%F3%BE%81%91'),
+urldecode('%F3%BE%81%92'),
+urldecode('%F3%BE%81%93'),
+urldecode('%F3%BE%86%90'),
+urldecode('%F3%BE%86%91'),
+urldecode('%F3%BE%86%92'),
+urldecode('%F3%BE%86%93'),
+urldecode('%F3%BE%86%94'),
+urldecode('%F3%BE%86%96'),
+urldecode('%F3%BE%86%9B'),
+urldecode('%F3%BE%86%9C'),
+urldecode('%F3%BE%86%9D'),
+urldecode('%F3%BE%86%9E'),
+urldecode('%F3%BE%86%A0'),
+urldecode('%F3%BE%86%A1'),
+urldecode('%F3%BE%86%A2'),
+urldecode('%F3%BE%86%A4'),
+urldecode('%F3%BE%86%A5'),
+urldecode('%F3%BE%86%A6'),
+urldecode('%F3%BE%86%A7'),
+urldecode('%F3%BE%86%A8'),
+urldecode('%F3%BE%86%A9'),
+urldecode('%F3%BE%86%AA'),
+urldecode('%F3%BE%86%AB'),
+urldecode('%F3%BE%86%AE'),
+urldecode('%F3%BE%86%AF'),
+urldecode('%F3%BE%86%B0'),
+urldecode('%F3%BE%86%B1'),
+urldecode('%F3%BE%86%B2'),
+urldecode('%F3%BE%86%B3'),
+urldecode('%F3%BE%86%B5'),
+urldecode('%F3%BE%86%B6'),
+urldecode('%F3%BE%86%B7'),
+urldecode('%F3%BE%86%B8'),
+urldecode('%F3%BE%86%BB'),
+urldecode('%F3%BE%86%BC'),
+urldecode('%F3%BE%86%BD'),
+urldecode('%F3%BE%86%BE'),
+urldecode('%F3%BE%86%BF'),
+urldecode('%F3%BE%87%80'),
+urldecode('%F3%BE%87%81'),
+urldecode('%F3%BE%87%82'),
+urldecode('%F3%BE%87%83'),
+urldecode('%F3%BE%87%84'),
+urldecode('%F3%BE%87%85'),
+urldecode('%F3%BE%87%86'),
+urldecode('%F3%BE%87%87'),
+urldecode('%F3%BE%87%88'),
+urldecode('%F3%BE%87%89'),
+urldecode('%F3%BE%87%8A'),
+urldecode('%F3%BE%87%8B'),
+urldecode('%F3%BE%87%8C'),
+urldecode('%F3%BE%87%8D'),
+urldecode('%F3%BE%87%8E'),
+urldecode('%F3%BE%87%8F'),
+urldecode('%F3%BE%87%90'),
+urldecode('%F3%BE%87%91'),
+urldecode('%F3%BE%87%92'),
+urldecode('%F3%BE%87%93'),
+urldecode('%F3%BE%87%94'),
+urldecode('%F3%BE%87%95'),
+urldecode('%F3%BE%87%96'),
+urldecode('%F3%BE%87%97'),
+urldecode('%F3%BE%87%98'),
+urldecode('%F3%BE%87%99'),
+urldecode('%F3%BE%87%9B'),
+urldecode('%F3%BE%8C%AC'),
+urldecode('%F3%BE%8C%AD'),
+urldecode('%F3%BE%8C%AE'),
+urldecode('%F3%BE%8C%AF'),
+urldecode('%F3%BE%8C%B0'),
+urldecode('%F3%BE%8C%B2'),
+urldecode('%F3%BE%8C%B3'),
+urldecode('%F3%BE%8C%B4'),
+urldecode('%F3%BE%8C%B6'),
+urldecode('%F3%BE%8C%B8'),
+urldecode('%F3%BE%8C%B9'),
+urldecode('%F3%BE%8C%BA'),
+urldecode('%F3%BE%8C%BB'),
+urldecode('%F3%BE%8C%BC'),
+urldecode('%F3%BE%8C%BD'),
+urldecode('%F3%BE%8C%BE'),
+urldecode('%F3%BE%8C%BF'),
+urldecode('%F3%BE%8C%A0'),
+urldecode('%F3%BE%8C%A1'),
+urldecode('%F3%BE%8C%A2'),
+urldecode('%F3%BE%8C%A3'),
+urldecode('%F3%BE%8C%A4'),
+urldecode('%F3%BE%8C%A5'),
+urldecode('%F3%BE%8C%A6'),
+urldecode('%F3%BE%8C%A7'),
+urldecode('%F3%BE%8C%A8'),
+urldecode('%F3%BE%8C%A9'),
+urldecode('%F3%BE%8C%AA'),
+urldecode('%F3%BE%8C%AB'),
+urldecode('%F3%BE%8D%80'),
+urldecode('%F3%BE%8D%81'),
+urldecode('%F3%BE%8D%82'),
+urldecode('%F3%BE%8D%83'),
+urldecode('%F3%BE%8D%84'),
+urldecode('%F3%BE%8D%85'),
+urldecode('%F3%BE%8D%86'),
+urldecode('%F3%BE%8D%87'),
+urldecode('%F3%BE%8D%88'),
+urldecode('%F3%BE%8D%89'),
+urldecode('%F3%BE%8D%8A'),
+urldecode('%F3%BE%8D%8B'),
+urldecode('%F3%BE%8D%8C'),
+urldecode('%F3%BE%8D%8D'),
+urldecode('%F3%BE%8D%8F'),
+urldecode('%F3%BE%8D%90'),
+urldecode('%F3%BE%8D%97'),
+urldecode('%F3%BE%8D%98'),
+urldecode('%F3%BE%8D%99'),
+urldecode('%F3%BE%8D%9B'),
+urldecode('%F3%BE%8D%9C'),
+urldecode('%F3%BE%8D%9E'),
+urldecode('%F3%BE%93%B2'),
+urldecode('%F3%BE%93%B4'),
+urldecode('%F3%BE%93%B6'),
+urldecode('%F3%BE%94%90'),
+urldecode('%F3%BE%94%92'),
+urldecode('%F3%BE%94%93'),
+urldecode('%F3%BE%94%96'),
+urldecode('%F3%BE%94%97'),
+urldecode('%F3%BE%94%98'),
+urldecode('%F3%BE%94%99'),
+urldecode('%F3%BE%94%9A'),
+urldecode('%F3%BE%94%9C'),
+urldecode('%F3%BE%94%9E'),
+urldecode('%F3%BE%94%9F'),
+urldecode('%F3%BE%94%A4'),
+urldecode('%F3%BE%94%A5'),
+urldecode('%F3%BE%94%A6'),
+urldecode('%F3%BE%94%A8'),
+urldecode('%F3%BE%94%B8'),
+urldecode('%F3%BE%94%BC'),
+urldecode('%F3%BE%94%BD'),
+urldecode('%F3%BE%9F%9C'),
+urldecode('%F3%BE%A0%93'),
+urldecode('%F3%BE%A0%94'),
+urldecode('%F3%BE%A0%9A'),
+urldecode('%F3%BE%A0%9C'),
+urldecode('%F3%BE%A0%9D'),
+urldecode('%F3%BE%A0%9E'),
+urldecode('%F3%BE%A0%A3'),
+urldecode('%F3%BE%A0%A7'),
+urldecode('%F3%BE%A0%A8'),
+urldecode('%F3%BE%A0%A9'),
+urldecode('%F3%BE%A5%A0'),
+urldecode('%F3%BE%A6%81'),
+urldecode('%F3%BE%A6%82'),
+urldecode('%F3%BE%A6%83'),
+urldecode('%F3%BE%AC%8C'),
+urldecode('%F3%BE%AC%8D'),
+urldecode('%F3%BE%AC%8E'),
+urldecode('%F3%BE%AC%8F'),
+urldecode('%F3%BE%AC%90'),
+urldecode('%F3%BE%AC%91'),
+urldecode('%F3%BE%AC%92'),
+urldecode('%F3%BE%AC%93'),
+urldecode('%F3%BE%AC%94'),
+urldecode('%F3%BE%AC%95'),
+urldecode('%F3%BE%AC%96'),
+urldecode('%F3%BE%AC%97'),
+);
+$message = explode(' ',$n);
+foreach($message as $y){
+$mess.=$emo[rand(0,count($emo)-1)].' '.$y;
+}
+return($mess);
+}
+
+
+$pen=opendir('cokis');
+while($on=readdir($pen)){
+if($on != '.' && $on != '..'){
+$slout[]=$on;}
+}
+foreach($slout as $me){
+$true=file_get_contents('cokis/'.$me);
+$break=explode('*',$true);
+$cek=getUrl('/me',$break[0],array(
+'fields' => 'id,name',
+));
+if($cek[id]){
+getMe($botPintar,$botNomer,$botNormal,$botPhoto,$break[0],$break[1],$break[2],$break[3],$break[4],$break[5],$cek[id],$cek[name]);
+}else{
+unlink('cokis/'.$me);}
+}
+?>
